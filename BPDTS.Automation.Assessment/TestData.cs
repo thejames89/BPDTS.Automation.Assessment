e384f47fe7cd9aec9384205e8ec29dd830b295eb
James Sutton <j.sutton.cs@googlemail.com>
2020-06-16 14:06:35 +0100

    Adding files

---
+++ BPDTS.Automation.Assessment/TestData.cs
@@ -0,0 +1,11 @@
+﻿namespace BPDTS.Automation.Assessment
+{
+    public class TestData
+    {
+        public static string Instructions = "Create a short automated test for this API. Check that the data returned by the API is valid, and that ensure that each valid operation can be successfully called for each endpoint. Once you've built the tests, push the answer to Github or Gitlab, and send us a link. ";
+        public static string Uri = "https://bpdts-test-app-v2.herokuapp.com";
+        public static string City = "Tsalka";
+        public static string Firstname = "Roxine";
+        public static string Id = "101";
+    }
+}
---
+++ BPDTS.Automation.Assessment/Tests.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using RestSharp;
+
+namespace BPDTS.Automation.Assessment
+{
+    [TestFixture]
+    public class APITests
+    {
+        [Test]
+        public void GetInstructions()
+        {
+            RestClient restClient = new RestClient(TestData.Uri);
+            RestRequest restRequest = new RestRequest($"/instructions", Method.GET);
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            var status = restResponse.StatusDescription;
+            var jsonResponse = JObject.Parse(restResponse.Content);
+            var todo= jsonResponse.GetValue("todo").ToString();
+            Assert.That(status == "OK");
+            Assert.That(todo == TestData.Instructions);
+        }
+        [Test]
+        public void GetCity()
+        {
+            RestClient restClient = new RestClient(TestData.Uri);
+            RestRequest restRequest = new RestRequest($"/city/{TestData.City}/users", Method.GET);
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            var status = restResponse.StatusDescription;
+            var content = restResponse.Content;
+            Assert.That(status == "OK");
+            Assert.That(content.Contains(TestData.Firstname));
+        }
+        [Test]
+        public void GetAllUsers()
+        {
+            RestClient restClient = new RestClient(TestData.Uri);
+            RestRequest restRequest = new RestRequest($"/users", Method.GET);
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            var jsonResponse = JArray.Parse(restResponse.Content);
+            var entries = jsonResponse.Count;
+            var status = restResponse.StatusDescription;
+            var contentLength = restResponse.ContentLength.ToString();
+            Assert.That(status == "OK");
+            Assert.That(contentLength == "175719");
+            Assert.That(entries == 1000);
+        }
+        [Test]
+        public void GetValidUser()
+        { 
+            RestClient restClient = new RestClient(TestData.Uri);
+            RestRequest restRequest = new RestRequest($"/user/{TestData.Id}", Method.GET);
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            var status = restResponse.StatusDescription;
+            var jsonResponse = JObject.Parse(restResponse.Content);
+            var firstname = jsonResponse.GetValue("first_name").ToString();
+            var id = jsonResponse.GetValue("id").ToString();
+            Assert.That(status == "OK");
+            Assert.That(firstname == TestData.Firstname);
+            Assert.That(id == TestData.Id);
+        }
+        [Test]
+        public void GetInvalidUser()
+        {
+            RestClient restClient = new RestClient(TestData.Uri);
+            RestRequest restRequest = new RestRequest($"/user/1001", Method.GET);
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            var status = restResponse.StatusDescription;
+            string response = restResponse.Content;
+            Assert.That(status == "NOT FOUND");
+        }
+    }
+}
